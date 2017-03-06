711ef34dd5e40f82d21f127b7b4988e97399edee
Guilherme Oliveira <guilherme.061@gmail.com>
2017-03-06 20:27:48 -0300

    Hearthstone Online Streamers List
    Work better on fonts and general design

--- real/hearthstonestreamers/assets/js/hearthstreamers.js
+++ real/hearthstonestreamers/assets/js/hearthstreamers.js
@@ -1,5 +1,5 @@
 //var streams = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
-var streams = ["disguisedtoasths", "savjz", "trumpsc", "playhearthstone", "esl_hearthstone", "itshafu", "lifecoach1981", "amazhs", "tesdey"]
+var streams = ["disguisedtoasths", "savjz", "trumpsc", "playhearthstone", "esl_hearthstone", "itshafu", "lifecoach1981", "amazhs", "kolento", "strifecro", "reynad27", "ratsmah", "bmkibler", "forsenlol", "nl_kripp", "tesdey"]
 
 var makeUrl = function(type, channel) {
   return ('https://wind-bow.gomix.me/twitch-api/'+type+'/'+channel+'?callback=?');
@@ -17,11 +17,11 @@ var makeObject = function(channel) {
         if(channelData.name === undefined) {
           $("#streaming").append("<p class='offline'><span class='streamer'>" + channel + "</span> <span class='status'>Doesn't exist</span></p>");
         } else {
-          $("#streaming").append("<p class='offline'><a target='blank' href='https://twitch.tv/"+channelData.name+"'><span class='streamer'>"+channelData.name+"</span></a> <span class='status'>Offline</span></p>");
+          $("#streaming").append("<p class='offline'><a target='blank' href='https://twitch.tv/"+channelData.name+"'><img class='streamerImg' src='"+channelData.logo+"'><span class='streamer'>"+channelData.name+"</span></a> <span class='status'>Offline</span></p>");
         }
       });
     }  else {
-      $("#streaming").append("<p class='online'><a target='blank' href='https://twitch.tv/"+data.stream.channel.name+"'><span class='streamer'>"+data.stream.channel.name+"</span></a> <span class='status'> "+data.stream.channel.status+"</span>.</p>");
+      $("#streaming").append("<p class='online'><a target='blank' href='https://twitch.tv/"+data.stream.channel.name+"'><img class='streamerImg' src='"+data.stream.channel.logo+"'><span class='streamer'>"+data.stream.channel.name+"</span></a> <span class='status'> "+data.stream.game + ": "+data.stream.channel.status+"</span></p>");
     }
   });
 }
@@ -29,20 +29,6 @@ var makeObject = function(channel) {
 var showStreams = function() {
   for(var i = 0; i < streams.length; i++) {
     makeObject(streams[i]);
-
-    /*
-    $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/'+streams[i]+'?callback=?', function(data) {
-      var infoString = "";
-
-      if(data.stream === null) {
-        infoString = "<p><span class='streamer'>" + streams[i] + "</span> is currently offline</p>";
-      } else {
-        infoString = "<p><span class='streamer'>"+streams[i]+"</span> is currently online playing "+data.stream.game+".</p>";
-      }
-
-      $("#streaming").append(infoString);
-
-    });*/
   }
 }
 
