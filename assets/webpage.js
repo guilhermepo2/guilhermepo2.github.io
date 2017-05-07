bdff3fa7c46bc78b8063e59099da42006f36d006
Guilherme Oliveira <guilherme.061@gmail.com>
2017-05-07 13:41:04 -0300

    agora vai

--- assets/webpage.js
+++ assets/webpage.js
@@ -1,7 +1,7 @@
 // selecting sections
 var workingSection = $("#working-section");
 var whatSection = $("#what-section");
-var projectsSection = $("#projects-section");
+var portfolioSection = $("#portfolio-section");
 var hobbiesSection = $("#hobbies-section");
 // selecting buttons
 var working = $("#working");
@@ -12,35 +12,35 @@ var hobbies = $("#hobbies");
 working.click(function(){
   workingSection.toggle();
   whatSection.hide();
-  projectsSection.hide();
+  .hide();
   hobbiesSection.hide();
 });
 
 what.click(function() {
   workingSection.hide();
   whatSection.toggle();
-  projectsSection.hide();
+  portfolioSection.hide();
   hobbiesSection.hide();
 });
 
 projects.click(function() {
   workingSection.hide();
   whatSection.hide();
-  projectsSection.toggle();
+  portfolioSection.toggle();
   hobbiesSection.hide();
 });
 
 hobbies.click(function() {
   workingSection.hide();
   whatSection.hide();
-  projectsSection.hide();
+  portfolioSection.hide();
   hobbiesSection.toggle();
 });
 
 var init = function() {
   workingSection.hide();
   whatSection.hide();
-  projectsSection.hide();
+  portfolioSection.hide();
   hobbiesSection.hide();
 }
 
