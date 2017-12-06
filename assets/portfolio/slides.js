9ee2683bcb062dcaad585d6f2516efedbf13b959
Guilherme Oliveira <guilherme.061@gmail.com>
2017-12-06 19:16:15 -0200

    portfolio games

---
+++ assets/portfolio/slides.js
@@ -0,0 +1,61 @@
+var workingOnShown = false;
+var postponedShown = false;
+var finishedShown = false;    
+
+$(document).ready(function() {
+    $("#workingOnContent").slideUp(1000);
+    $("#postponedContent").slideUp(1000);
+    $("#finishedContent").slideUp(1000);    
+
+    var myShow = function(content, icon) {
+        $(content).slideDown(1000);
+        $(icon).removeClass();
+        $(icon).addClass("fa fa-caret-down");
+    }
+
+    var myHide = function(content, icon) {
+        $(content).slideUp(1000);
+        $(icon).removeClass();
+        $(icon).addClass("fa fa-caret-right");
+    }
+
+    $("#workingButton").click(function() {
+        if(!workingOnShown) {
+            myShow("#workingOnContent", "#workingOnIcon");
+            myHide("#postponedContent", "#postponedIcon");
+            postponedShown = false;
+            myHide("#finishedContent", "#finishedIcon");
+            finishedShown = false;
+        } else {
+            myHide("#workingOnContent", "#workingOnIcon");
+        }
+        workingOnShown = !workingOnShown;
+    });
+
+    $("#postponedButton").click(function() {
+        if(!postponedShown) {
+            myShow("#postponedContent", "#postponedIcon");
+            myHide("#workingOnContent", "#workingOnIcon");
+            workingOnShown = false;
+            myHide("#finishedContent", "#finishedIcon");
+            finishedShown = false;
+        } else {
+            myHide("#postponedContent", "#postponedIcon");
+        }
+        postponedShown = !postponedShown;
+    });
+
+    $("#finishedButton").click(function() {
+        if(!finishedShown) {
+            myShow("#finishedContent", "#finishedIcon");
+            myHide("#workingOnContent", "#workingOnIcon");
+            workingOnShown = false;
+            myHide("#postponedContent", "#postponedIcon");
+            postponedShown = false;
+        } else {
+            myHide("#finishedContent", "#finishedIcon");
+        }
+
+        finishedShown = !finishedShown;
+    });
+});
